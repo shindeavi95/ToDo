b24d89111c40fbf8b6e2f2c56c4af4edc4270fa5
Aviansh <avinashshinde999.as@gmail.com>
2020-12-02 20:28:53 +0530

    This is Third commit By avinash

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
+from .form import *
+
 
 def index(request):
     tasks = Task.objects.all()
-    data= {'tasks':tasks}
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+
+    data= {'tasks':tasks, 'form':form}
     return render(request,'tasks/list.html',data)
+
+def updateTask(request, pk):
+
+             task = Task.objects.get(id=pk)
+             form = TaskForm(instance=task)
+             if request.method == 'POST':
+                 form = TaskForm(request.POST, instance=task)
+                 if form.is_valid():
+                     form.save()
+                    return redirect('/')
+             data = {'form':form}
